b1b03e4264af54a0c654709c51a01229f6c960e0
Leandro Martins <leandro.marttins.85@gmail.com>
2024-01-31 18:20:19 -0300

    Configurando Styled Components e Design System

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { UserProvider } from './contexts/UserContext'
 import { Home } from './pages/Home'
+import { GlobalStyle } from './styles/global'
 
 function App() {
   return (
     <UserProvider>
+      <GlobalStyle />
       <Home />
     </UserProvider>
   )
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,22 @@
+export const defaultTheme = {
+  white: '#fff',
+
+  'base-input': '#040F1A',
+  'base-background': '#071422',
+  'base-profile': '#0B1B2B',
+  'base-post': '#112131',
+  'base-border': '#1C2F41',
+  'base-label': '#3A536B',
+  'base-span': '#7B96B2',
+  'base-subtitle': '#AFC2D4',
+  'base-title': '#E7EDF4',
+
+  blue: '#3294F8',
+
+  'font-sm': '0.75rem', // 12px
+  'font-s': '0.875rem', // 14px
+  'font-normal': '1rem', // 16px
+  'font-m': '1.125rem', // 18px
+  'font-l': '1.25rem', // 20px
+  'font-xl': '1.5rem', // 24px
+} as const
